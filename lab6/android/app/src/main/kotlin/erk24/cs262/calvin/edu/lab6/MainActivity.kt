65c7c66aa04f6192bc7c8a0d946f30782cb391b1
kodingkoning <erk24@maroon14.cs.calvin.edu>
2019-10-11 16:17:58 -0400

    Lab 6 and screenshot

---
+++ lab6/android/app/src/main/kotlin/erk24/cs262/calvin/edu/lab6/MainActivity.kt
@@ -0,0 +1,13 @@
+package erk24.cs262.calvin.edu.lab6
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
